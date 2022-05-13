bac7304f61b6c4b23aec6f4bfa99982aa12bbebd
Ben Osborne <hello@benosborne.io>
2022-05-13 19:59:51 +0100

    add get() to Environment.php

--- tests/Foundation/EnvironmentTest.php
+++ tests/Foundation/EnvironmentTest.php
@@ -7,12 +7,33 @@ use Restolia\Foundation\Environment;
 
 class EnvironmentTest extends TestCase
 {
+    private Environment $environment;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->environment = new Environment(dirname(__DIR__) . '/Foundation/data/');
+    }
+
     public function testIs(): void
     {
-        $environment = new Environment(dirname(__DIR__) . '/Foundation/data/');
-        $this->assertTrue($environment->is('testing'));
-        $this->assertFalse($environment->is('local'));
-        $this->assertTrue($environment->is('testing', 'local'));
-        $this->assertTrue($environment->is('local', 'testing'));
+        $this->assertTrue($this->environment->is('testing'));
+        $this->assertFalse($this->environment->is('local'));
+        $this->assertTrue($this->environment->is('testing', 'local'));
+        $this->assertTrue($this->environment->is('local', 'testing'));
+    }
+
+    public function testGet(): void
+    {
+        $this->assertEquals('testing', $this->environment->get('APP_ENV'));
+    }
+
+    public function testGetWithDefault(): void
+    {
+        $this->assertEquals('foo', $this->environment->get('UNKNOWN', 'foo'));
+        $this->assertTrue($this->environment->get('UNKNOWN', true));
+        $this->assertFalse($this->environment->get('UNKNOWN', false));
+        $this->assertNull($this->environment->get('UNKNOWN'));
     }
 }
